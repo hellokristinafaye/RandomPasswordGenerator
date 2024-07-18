6b4e80d939a077699c60954635cead89a792d459
Kristina Faye <hellokristinafaye@gmail.com>
2024-07-17 21:02:56 -0700

    started JS! declared some variables and started function generatePassword()

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello there!");
+let generateButton = document.getElementById('generateButton');
+let passwordInput = document.getElementById('password-input');
+
+function generatePassword() {
+    let password = ""
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123!@#$%^&*()_+"
+
+    for (let i = 0; i < 8; i++) {
+        password += 
+    }
+}
